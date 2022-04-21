89f4ed379971d4d6832c98b56dabfa5366e27271
fan daily <dailysfan@163.com>
2022-04-21 16:20:15 +0800

    master

--- Sort Algorithm/Bubble Sort/BubbleSort.cpp
+++ Sort Algorithm/Bubble Sort/BubbleSort.cpp
@@ -1,4 +1,12 @@
 #include "BubbleSort.h"
 
 template<typename T>
-void 
+void BubbleSort(T& data, int size){
+    for(int i = 0; i < size-1; i++){
+        for(int j = 0; j < size-1-i; j++){
+            if(data[i] < data[j]){
+                
+            }
+        }
+    }
+}
